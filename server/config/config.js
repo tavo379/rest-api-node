5713fa24d0bdc084894ed9a5d0e6183c2625d021
tavoramirez379 <tavoramirez379@gmail.com>
2018-10-19 13:53:42 -0500

    added enviroment variable mongo

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if( process.env.NODE_ENV  === 'dev' ) {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = process.env.MONG_URI;
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
